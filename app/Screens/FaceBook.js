0b9d4bbfaf11b8f8d6601d3dc7388913537ec5c6
lucalas <luca_taddeo@hotmail.it>
2018-03-26 01:45:50 +0200

    Facebook page based on WebView
    First implementation of facebook page based on WebView that could be changed using a custom layout in future

--- app/Screens/FaceBook.js
+++ app/Screens/FaceBook.js
@@ -1,27 +1,36 @@
 import React, { Component } from "react";
 
-import { Platform, StyleSheet, Text, View } from "react-native";
+import { Platform, StyleSheet, Text, View, WebView, ActivityIndicator } from "react-native";
+
+import Config from 'react-native-config';
 
 export default class FaceBook extends Component<Props> {
+
+  loaderFBView() {
+    return <ActivityIndicator
+            size="large"
+            color="#0000ff"
+            hidesWhenStopped={true}
+            animating={true}
+            style={style.body}
+          />
+  }
+
   render() {
+    // TODO Maybe we can implements a custom view to show page instead of open webview to show facebook site page
     return (
-      <View style={style.view}>
-        <Text>COMING SOON</Text>
-      </View>
+        <WebView
+        source={{uri: Config.radiofbpage}}
+        renderLoading={this.loaderFBView} startInLoadingState={true}
+        />
     );
   }
 }
 
 
 const style = StyleSheet.create({
-  view: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "#464848"
-  },
   body: {
-    flex: 6,
+    flex: 1,
     flexDirection: "column",
     justifyContent: "center",
     alignItems: "center"
